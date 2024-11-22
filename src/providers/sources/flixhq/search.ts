9dd6f264e8f3b6738632257c851200caa6efe0d2
steveyout <youtsteve1@gmail.com>
2024-11-22 16:07:08 +0300

    added a new source

--- src/providers/sources/flixhq/search.ts
+++ src/providers/sources/flixhq/search.ts
@@ -1,15 +1,12 @@
-import { load } from 'cheerio';
-import { MOVIES } from 'wikiextensions-flix';
-
 import { MovieMedia, ShowMedia } from '@/entrypoint/utils/media';
-import { flixHqBase } from '@/providers/sources/flixhq/common';
 import { compareMedia, compareTitle } from '@/utils/compare';
 import { ScrapeContext } from '@/utils/context';
 
 export async function getFlixhqId(ctx: ScrapeContext, media: MovieMedia | ShowMedia): Promise<string | null> {
-  const flixhq = new MOVIES.FlixHQ();
-  const search = await flixhq.search(media.title.replaceAll(/[^a-z0-9A-Z]/g, '-'));
-  const items = search.results.map((result) => {
+  const search = await ctx.proxiedFetcher(`/search?query=${media.title.replaceAll(/[^a-z0-9A-Z]/g, '-')}`, {
+    baseUrl: 'https://youplex.site/api',
+  });
+  const items = search.results.map((result: { id: string; title: string; releaseDate: string; seasons: number }) => {
     const id = result.id;
     const title = result.title;
     const year = result.releaseDate ? result.releaseDate : 'uknown';
@@ -23,14 +20,13 @@ export async function getFlixhqId(ctx: ScrapeContext, media: MovieMedia | ShowMe
     };
   });
 
-  const matchingItem = items.find((v) => {
+  const matchingItem = items.find((v: { title: string; year: number | undefined; seasons: number }) => {
     if (!v) return false;
 
     if (media.type === 'movie') {
       return compareMedia(media, v.title, v.year);
     }
 
-    // @ts-ignore
     return compareTitle(media.title, v.title) && media.season.number < v.seasons + 1;
   });
 
