3c95c8791327fecbd76939e47c348ad1f99b6d25
steveyout <youtsteve1@gmail.com>
2026-02-22 12:35:40 +0300

    updated

--- src/providers/sources/youplex/index.ts
+++ src/providers/sources/youplex/index.ts
@@ -2,7 +2,6 @@ import { flags } from '@/entrypoint/utils/targets';
 import { SourcererOutput, makeSourcerer } from '@/providers/base';
 import { MovieScrapeContext, ShowScrapeContext } from '@/utils/context';
 import { NotFoundError } from '@/utils/errors';
-import { createM3U8ProxyUrl } from '@/utils/proxy';
 
 // 1. Stealth Name Pool - Randomized every time the app loads
 const stealthNames = [
@@ -56,7 +55,7 @@ async function youPlexBridge(ctx: ShowScrapeContext | MovieScrapeContext, scrape
         {
           id: 'primary',
           type: 'hls',
-          playlist: createM3U8ProxyUrl(res.url),
+          playlist: res.url,
           flags: [flags.CORS_ALLOWED],
           captions: (res.subtitles || []).map((s: any) => ({
             id: s.url,
